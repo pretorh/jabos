1493a8259b44cabe4788cac431f30d685a3e0e31
Hendri Pretorius <pretorh@gmail.com>
2013-10-24 22:16:05 +0200

    google drive create file (resumable)

---
+++ google-drive/create.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+set -e
+set -o pipefail
+
+if [ -z "$3" ] ; then
+    echo "Need: access_token parent_id file [title]"
+    exit 1
+fi
+
+access_token=$1
+parent_id=$2
+file=$3
+title=$4
+if [ -z "$title" ] ; then title=$file; fi
+file_size=$(stat -c%s "$file")
+
+data='{"title":"'$title'","parents":[{"id": "'$parent_id'"}]}'
+echo $data 2>&1
+
+curl https://www.googleapis.com/upload/drive/v2/files?uploadType=resumable \
+    -H "Authorization: Bearer $access_token" \
+    -H "Content-Type: application/json; charset=UTF-8" \
+    -H "X-Upload-Content-Length: $file_size" \
+    --data "$data" \
+    -i \
+    --progress-bar \
+    | grep "Location" \
+    | sed "s/Location: \(.*\)$/\1/"
---
+++ tests/google-drive/create-test.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+set -e
+
+access_token=`xmlstarlet sel -t -v "/settings/google/access-token" ../testdata.xml`
+echo "using access token $access_token"
+parent_id=`xmlstarlet sel -t -v "/settings/google/upload-to-id" ../testdata.xml`
+echo "using parent id $parent_id"
+
+url=`../../google-drive/create.sh $access_token $parent_id create-test.sh 'create test shell script'`
+if [ -z "$url" ] ; then
+    echo "no url returned"
+    exit 1
+fi
+echo "uploading via $url"
