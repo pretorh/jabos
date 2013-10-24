390a5e1dddc0317e302e4bc456cb8176728a907f
Hendri Pretorius <pretorh@gmail.com>
2013-10-24 19:57:38 +0200

    test helper can remove log files cmd arg to remove old log files before and/or after running tests

--- test-all.sh
+++ test-all.sh
@@ -1,2 +1,13 @@
+if [ "$1" == "--clean" ] || [ "$1" == "--clean-after" ] ; then
+    echo "removing old log files"
+    find -name \*.log -print0 | xargs -0 rm
+fi
+
 ./btester/run.sh
+success=$?
 mv summary*.log tests/
+
+if [ "$1" == "--clean-after" ] && [ $success == 0 ] ; then
+    echo "removing all log files"
+    find -name \*.log -print0 | xargs -0 rm
+fi
